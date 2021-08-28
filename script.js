4fadbec71b486f1d954c6a94e9c23380ffb3107f
tylers22 <tylersharp1@gmail.com>
2021-08-27 21:21:33 -0700

    first function works

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function generate() {
- 
+var passwordPrompts = function () {
+      // Length of password prompt, 8-128 characters for prompts check meater reference in activity 1
+      var charCount = prompt('How many characters would you like to use in your password?  Please choose between 8 and 128 characters.');
+        console.log(charCount)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+      // Lowercase characters prompt
+      var confirmLower = confirm('Would you like to include lowercase characters?')
+        console.log(confirmLower)
 
-  passwordText.value = password;
+      // Uppercase characters prompt
+      var confirmUpper = confirm('Would you like to include uppercase characters?')
+        console.log(confirmUpper)
 
-}
+      // Numbers characters prompt
+      var confirmNumber = confirm('Would you like to include numbers?')
+        console.log(confirmNumber)
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      // Special characters prompt
+      var confirmSpecial = confirm('Would you like to include special characters?')
+        console.log(confirmSpecial)
 
+};
+
+passwordPrompts ();
 
